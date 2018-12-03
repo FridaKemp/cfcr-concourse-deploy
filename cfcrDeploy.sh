090fe9ac4fae86146205d7597bfebb002463a459
mercyck <mercedes@gmail.com>
2018-12-03 14:10:43 +0000

    removing vars

--- cfcrDeploy.sh
+++ cfcrDeploy.sh
@@ -3,15 +3,13 @@
 set -e # fail fast
 set -x # print commands
 
-api-hostname-cfcr=$1
-
 git clone cfcr-concourse-deploy updated-cfcr-concourse-deploy
 cd updated-cfcr-concourse-deploy
 
 bosh -n deploy -d cfcr-test cfcr-manifest.yml \
  -o single-master.yml \
  -o add-hostname-to-master-certificate.yml \
- -v api-hostname=api-hostname-cfcr \
+ -v api-hostname=api-hostname \
  -o local-config-server.yml \
  --vars-store generated.yml
 
